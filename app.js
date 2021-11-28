b73084cf6e543c1d7f243252b30711b318142267
SLDowney <sdowney@outlook.com>
2021-11-28 19:27:03 -0330

    JS file fully functional. Formatting of the HTML and CSS file updated.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Function to change Header Font
 const changeHeader = () => {
 	let headerOption = $("#header").val();
 
@@ -14,19 +15,35 @@ const changeHeader = () => {
 	}
 } // end of change header font function
 
+// Function to change Paragraph Font
 const changeParagraph = () => {
 	let paragraphOptions = $("#paragraph").val();
 
 	if ( paragraphOptions == "1" ) {
 		$("p").css("font-family", "'Kunstler Script', Helvetica, sans-serif");
-	}
+        $("p").css("font-size", "225%");
+    }
 	if ( paragraphOptions == "2" ) {
 		$("p").css("font-family", "'Bookman Old Style', Times, serif");
+        $("p").css("font-size", "100%");
 	}
 	if ( paragraphOptions == "3" ) {
 		$("p").css("font-family", "'Bookshelf Symbol 7', Times, serif");
+        $("p").css("font-size", "125%");
 	}
 } // end of change paragraph font function
+
+// Function to reset back to default settings
+const reset = () => {
+    $("#header").val("");
+    $("#paragraph").val("");
+    
+    $("p").css("font-family", "Arial, Helvetica, sans-serif");
+	$("p").css("font-size", "100%");
+
+    $("h1").css("font-family", "'Arial', Helvetica, sans-serif");
+	$("h1").css("font-size", "200%");
+} // end of Reset function
 
 $(document).ready(function() {
     $("#header").click( () => {
@@ -36,5 +53,9 @@ $(document).ready(function() {
 	$("#paragraph").click( () => {
         changeParagraph();
 	});
+
+    $("#reset").click( () => {
+        reset();
+    })
 
 });
